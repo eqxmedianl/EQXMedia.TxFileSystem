aa931839d307959b89d38c3f315bcaa3df0fdd28
Jarno Kamminga <jarno.kamminga@eqx-media.nl>
2021-04-25 21:32:35 +0200

    Added three minimal <inheritdoc/> XML comments

--- TxFileSystem/TxFileSystemWatcher.cs
+++ TxFileSystem/TxFileSystemWatcher.cs
@@ -29,16 +29,19 @@
         /// <include file="../Documentation/XmlDoc/TxFileSystem.XmlDoc.Extensions.xml" path='TxFileSystem.BaseDocs/Extensions/Classes/Class[@property="TxFileSystem"]/*' />
         internal TxFileSystem TxFileSystem { get; set; }
 
+        /// <inheritdoc cref="System.IO.FileSystemWatcher()" />
         public IFileSystemWatcher CreateNew()
         {
             return this.TxFileSystem.FileSystem.FileSystemWatcher.CreateNew();
         }
 
+        /// <inheritdoc cref="System.IO.FileSystemWatcher(string)" />
         public IFileSystemWatcher CreateNew(string path)
         {
             return this.TxFileSystem.FileSystem.FileSystemWatcher.CreateNew(path);
         }
 
+        /// <inheritdoc cref="System.IO.FileSystemWatcher(string, string)" />
         public IFileSystemWatcher CreateNew(string path, string filter)
         {
             return this.TxFileSystem.FileSystem.FileSystemWatcher.CreateNew(path, filter);
