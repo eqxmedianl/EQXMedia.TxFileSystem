8a93300e8fd308e2b94bb248be02497d311b50de
Jarno Kamminga <jarno.kamminga@eqx-media.nl>
2021-04-25 22:55:17 +0200

    Removed unused using statement

--- TxFileSystem/TxFile.cs
+++ TxFileSystem/TxFile.cs
@@ -1,6 +1,5 @@
 ﻿namespace EQXMedia.TxFileSystem
 {
-    using global::EQXMedia.TxFileSystem.Abstractions;
     using global::EQXMedia.TxFileSystem.Operations.Files;
     using System;
     using System.Collections.Generic;
