3b967d3c515ba83b17438770614da32a427842ce
Jarno Kamminga <jarno.kamminga@eqx-media.nl>
2021-04-26 15:22:20 +0200

    Added a Unit Test to test TxFileSystem's constructor exception

---
+++ TxFileSystem.Tests/TxFileSystem_Should.cs
@@ -0,0 +1,17 @@
+﻿namespace EQXMedia.TxFileSystem.Tests
+{
+    using EQXMedia.TxFileSystem.Exceptions;
+    using Xunit;
+
+    public sealed class TxFileSystem_Should
+    {
+        [Fact]
+        public void TxFileSystem_TxFileSystemPassed_ThrowsUnsupportedFileSystemImplementationException()
+        {
+            Assert.Throws<UnsupportedFileSystemImplementationException>(() =>
+            {
+                new TxFileSystem(new TxFileSystem());
+            });
+        }
+    }
+}
