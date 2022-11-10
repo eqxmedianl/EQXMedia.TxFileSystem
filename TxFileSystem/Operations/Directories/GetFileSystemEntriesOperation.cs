d0c9e1da30044043f85486c5f4013996bf626b2b
Jarno Kamminga <jarno.kamminga@eqx-media.nl>
2022-11-10 21:44:30 +0100

    Upgraded System.IO.Abstractions to version 17.2.3

--- TxFileSystem/Operations/Directories/GetFileSystemEntriesOperation.cs
+++ TxFileSystem/Operations/Directories/GetFileSystemEntriesOperation.cs
@@ -1,10 +1,13 @@
 ﻿namespace EQXMedia.TxFileSystem.Operations.Directories
 {
     using global::EQXMedia.TxFileSystem.Abstractions;
+    using System;
+    using System.IO;
 
     internal sealed class GetFileSystemEntriesOperation : DirectoryOperation, IReturningOperation<string[]>
     {
         private readonly string _searchPattern = null;
+        private readonly Nullable<SearchOption> _searchOption = null;
 
         public GetFileSystemEntriesOperation(TxDirectory directory, string path)
             : base(directory, path)
@@ -15,6 +18,12 @@
             : this(directory, path)
         {
             _searchPattern = searchPattern;
+        }
+
+        public GetFileSystemEntriesOperation(TxDirectory directory, string path, string searchPattern, SearchOption searchOption)
+            : this(directory, path, searchPattern)
+        {
+            _searchOption = searchOption;
         }
 
         public override OperationType OperationType => OperationType.Info;
@@ -28,7 +37,12 @@
                 return _directory.TxFileSystem.FileSystem.Directory.GetFileSystemEntries(_path);
             }
 
-            return _directory.TxFileSystem.FileSystem.Directory.GetFileSystemEntries(_path, _searchPattern);
+            if (!_searchOption.HasValue)
+            {
+                return _directory.TxFileSystem.FileSystem.Directory.GetFileSystemEntries(_path, _searchPattern);
+            }
+
+            return _directory.TxFileSystem.FileSystem.Directory.GetFileSystemEntries(_path, _searchPattern, _searchOption.Value);
         }
     }
 }
